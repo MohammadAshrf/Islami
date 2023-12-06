02affa06c253580375064f52620076c8b6a3c8d0
MohammadAshrf <mohamedashrf155@gmail.com>
2023-12-07 01:45:17 +0200

    + Hadeth Details + Hadeth Tab

--- app/src/main/java/com/example/islami/Constants.kt
+++ app/src/main/java/com/example/islami/Constants.kt
@@ -1,6 +1,11 @@
 package com.example.islami
 
 object Constants {
+    const val SOBHAN_ALLAH = "سـبـحـان الله"
+    const val AL_HAMDO_ALLAH = "الـحـمـد لله"
+    const val ALLAH_AKBAR = "الله أكـبـر"
+    const val AL_Khatema =
+        "لا إلـه إلا الله وحـده لا شـريـك له، له الملك وله الحمد وهو على كل شئ قدير"
     const val EXTRA_HADETH = "hadeth"
     const val EXTRA_CHAPTER_INDEX = "index"
     const val EXTRA_CHAPTER_NAME = "name"
